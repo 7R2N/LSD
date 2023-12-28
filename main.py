9b4b9ac18fafdf309964d48550104ee5c22c8369
7R2N <maksymilian.l.bartosik@gmail.com>
2023-12-28 12:58:25 +0100

    Changes to be committed: new file:   main.py 	new file:   src/__pycache__/com.cpython-38.pyc 	modified:   src/com.py 	new file:   src/plotter.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from src import com
+
+if __name__ == "__main__":
+    com.serial_collector()
--- src/com.py
+++ src/com.py
@@ -1,32 +1,33 @@
 import serial
 import time
 
-s = serial.Serial('COM5', 9600, timeout=1)
+def serial_collector():
+    s = serial.Serial('COM5', 9600, timeout=1)
 
-data = []
+    data = []
 
-while 1:
-    for i in range(2):
-        row = []
-        s.readline().decode("utf-8")
-        volt = s.readline().decode("utf-8")
-        print(volt)
-        svolt = volt.split(sep=';')
-        
-        psv = float(svolt[0])
-        pdv = float(svolt[1])
-        
-        print("PSV " + str(psv))
-        print("PDV " + str(pdv))
+    while 1:
+        for i in range(2):
+            row = []
+            s.readline().decode("utf-8")
+            volt = s.readline().decode("utf-8")
+            print(volt)
+            svolt = volt.split(sep=';')
+            
+            psv = float(svolt[0])
+            pdv = float(svolt[1])
+            
+            print("PSV " + str(psv))
+            print("PDV " + str(pdv))
 
-        dvolt = [psv, pdv]
+            dvolt = [psv, pdv]
 
-        print(dvolt)
+            print(dvolt)
 
-        row.append(dvolt)
+            row.append(dvolt)
 
-    data.append(row)
+        data.append(row)
 
-    print(data)
+        print(data)
 
-    time.sleep(1)
+        time.sleep(1)
