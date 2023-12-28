7c6942ee45faabfe480c5820f308e2d590d4f8d0
7R2N <maksymilian.l.bartosik@gmail.com>
2023-12-28 12:38:26 +0100

    improving data collecting

--- src/com.py
+++ src/com.py
@@ -3,13 +3,30 @@ import time
 
 s = serial.Serial('COM5', 9600, timeout=1)
 
+data = []
+
 while 1:
-    s.readline().decode("utf-8")
-    volt = s.readline().decode("utf-8")
-    print(volt)
-    svolt = volt.split(sep=';')
-    
-    print("PSV " + svolt[0])
-    print("PDV " + svolt[1])
+    for i in range(2):
+        row = []
+        s.readline().decode("utf-8")
+        volt = s.readline().decode("utf-8")
+        print(volt)
+        svolt = volt.split(sep=';')
+        
+        psv = float(svolt[0])
+        pdv = float(svolt[1])
+        
+        print("PSV " + str(psv))
+        print("PDV " + str(pdv))
+
+        dvolt = [psv, pdv]
+
+        print(dvolt)
+
+        row.append(dvolt)
+
+    data.append(row)
+
+    print(data)
 
     time.sleep(1)
